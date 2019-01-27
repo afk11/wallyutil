cb620a59f1774880580c42d1ce29a994a0162569
Thomas Kerin <thomas.kerin@bitmaintech.com>
2019-01-27 00:16:04 +0000

    seems working now for desired script types

--- src/wallyutil.c
+++ src/wallyutil.c
@@ -1,5 +1,6 @@
 #include <wally_core.h>
 #include <wally_crypto.h>
+#include <wally_script.h>
 #include <wally_bip39.h>
 #include <wally_bip32.h>
 #include <secp256k1.h>
@@ -18,10 +19,6 @@ int mygetline(char line[], int maxline)
     for (i = 0; i < maxline-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         line[i] = c;
     }
-//  if (c == '\n') {
-//	line[i] = c;
-//	i++;
-//    }
     line[i] = '\0';
     return i;
 }
@@ -31,15 +28,19 @@ void parse_xpub(struct ext_key* bip32key) {
     int len;
     
     while(1) {
-  	printf("enter xpub:");
+  	printf("enter xpub:\n");
 	len = mygetline(xpub, 113);
 	if (WALLY_OK == bip32_key_from_base58(xpub, bip32key)) {
 	    return;
 	}
-	printf("invalid xpub");
+	printf("invalid xpub, try again:\n");
     }
 }
-
+void print_hex(unsigned char* data, int len) {
+    for (int i = 0; i < len; i++) {
+	printf("%02x", data[i]);
+    }
+}
 int main(int argc, char** argv) {
     if (WALLY_OK != wally_init(0)) {
         return exit_error("couldn't init libwally");
@@ -58,13 +59,13 @@ int main(int argc, char** argv) {
         printf("usage: \n");
         return 0;
     }
-    
+
     printf("cmd'%s'\n",argv[1]);
     if (0 == strcmp(argv[1], "bip32-multisig")) {
-  	if (argc < 3) {
+  	if (argc < 4) {
 	    return exit_error("m and n required as arguments\n");
 	}
-	int m, n;
+	int m, n, i, j;
 	char *end;
 	m = strtol(argv[2], &end, 10);
 	if (m == 0) {
@@ -74,39 +75,92 @@ int main(int argc, char** argv) {
 	if (n == 0) {
 	    return exit_error("invalid value for num keys");
 	}
+	int sort = 0;
+	for (i = 4; i < argc; i++) {
+	    if (strcmp(argv[i], "--sort") == 0) {
+		sort = 1;
+	    } else {
+		return exit_error("unknown flag");
+	    }
+	}
 	struct ext_key key[n];
 	unsigned char* script_keys[n];
-	for (int i = 0; i < n; i++) {
+	for (i = 0; i < n; i++) {
 	    printf("please enter xpub %d\n", i);
 	    parse_xpub(&key[i]);
 	    script_keys[i] = key[i].pub_key;
-            for (int z = 0; z < 33; z++) {
-		printf("%02x", key[i].pub_key[z]);
-	    }
-	    printf("\n");
         }
-	int i = 0, j;
-//	unsigned char* tmp;
-//	for (i; i < n; i++) {
-//	    printf("sorted pubkey %d\n", i);
-//	    for (j = 0; j < n; j++) {
-//		printf("%s %d\n", script_keys[i], i);
-//	       	printf("%s %d\n", script_keys[j], j);
-//		if (strcmp(script_keys[i], script_keys[j]) < 0) {
-//		    tmp = script_keys[i];
-//		    script_keys[i] = script_keys[j];
-//		    script_keys[j] = tmp;
-//		}
-//	    }
-//	}
-	printf("multisig %d of %d\n", m, n);
-	for (i = 0; i < n; i++) {
-	    printf("pos %d\n", i);
-	    for (j = 0; j < 33; j++) {
-   	        printf("%02x", script_keys[i][j]);
+	if (sort) {
+   	    unsigned char* tmp;
+	    for (i = 0; i < n; i++) {
+	        for (j = 0; j < n; j++) {
+		    if (strcmp(script_keys[i], script_keys[j]) < 0) {
+		        tmp = script_keys[i];
+		        script_keys[i] = script_keys[j];
+		        script_keys[j] = tmp;
+		    }
+	        }
 	    }
-	    printf("\n");
 	}
+
+        unsigned char* pubkey_bytes = malloc(n*EC_PUBLIC_KEY_LEN);
+	for (i = 0; i < n; i++) {
+	    memcpy(pubkey_bytes+(i*EC_PUBLIC_KEY_LEN), script_keys[i], EC_PUBLIC_KEY_LEN);
+	}
+	size_t script_len = 1 + n*(1 + EC_PUBLIC_KEY_LEN) + 2;
+        unsigned char* script = malloc(script_len);
+	if (WALLY_OK != wally_scriptpubkey_multisig_from_bytes(pubkey_bytes, n*EC_PUBLIC_KEY_LEN,
+			m, 0, script, script_len, &script_len)) {
+	    return exit_error("failed to create multisig script");
+	}
+
+	unsigned char* script_p2sh = malloc(WALLY_SCRIPTPUBKEY_P2SH_LEN);
+	size_t written;
+	if (WALLY_OK != wally_scriptpubkey_p2sh_from_bytes(script, script_len,
+				WALLY_SCRIPT_HASH160, script_p2sh, WALLY_SCRIPTPUBKEY_P2SH_LEN, &written)) {
+	    return exit_error("failed to create multisig-p2sh script");
+	}
+
+	unsigned char* script_p2wsh = malloc(WALLY_SCRIPTPUBKEY_P2WSH_LEN);
+	if (WALLY_OK != wally_witness_program_from_bytes(script, script_len,
+				WALLY_SCRIPT_SHA256, script_p2wsh, WALLY_SCRIPTPUBKEY_P2WSH_LEN, &written)) {
+	    return exit_error("failed to create multisig-p2wsh script");
+	}
+
+	unsigned char* script_p2wsh_p2sh = malloc(WALLY_SCRIPTPUBKEY_P2SH_LEN);
+	if (WALLY_OK != wally_scriptpubkey_p2sh_from_bytes(script_p2wsh, WALLY_SCRIPTPUBKEY_P2WSH_LEN,
+				WALLY_SCRIPT_HASH160, script_p2wsh_p2sh, WALLY_SCRIPTPUBKEY_P2SH_LEN, &written)) {
+	    return exit_error("failed to create multisig-p2wsh-p2sh script");
+	}
+
+	printf("## multisig\n");
+	print_hex(script, script_len);
+        printf("\n");
+
+	printf("## p2sh(multisig)\n");
+	print_hex(script_p2sh, WALLY_SCRIPTPUBKEY_P2SH_LEN);
+	printf("\n");
+	print_hex(script, script_len);
+	printf("\n");
+
+	printf("## p2wsh(multisig)\n");
+	print_hex(script_p2wsh, WALLY_SCRIPTPUBKEY_P2WSH_LEN);
+	printf("\n");
+	print_hex(script, script_len);
+	printf("\n");
+
+        printf("## p2sh(p2wsh(multisig))\n");
+	print_hex(script_p2wsh_p2sh, WALLY_SCRIPTPUBKEY_P2SH_LEN);
+	printf("\n");
+	print_hex(script_p2wsh, WALLY_SCRIPTPUBKEY_P2WSH_LEN);
+	printf("\n");
+	print_hex(script, script_len);
+	printf("\n");
+	free(pubkey_bytes);
+	free(script);
+	free(script_p2sh);
+	free(script_p2wsh);
+	free(script_p2wsh_p2sh);
     } else {
         return exit_error("usage..");
     }
